e3f2270c3539a6a9783617a3a8f05fd954308464
Yefferson0312 <yeffersonandrey12@gmail.com>
2026-05-21 15:15:54 -0500

    Implementado HU-21 signos vitales

--- app/main.py
+++ app/main.py
@@ -41,8 +41,8 @@ def read_root():
 # from app.modules.historias_clinicas.routes import router as historias_clinicas_router
 # app.include_router(historias_clinicas_router, prefix="/historias_clinicas", tags=["historias_clinicas"])
 
-# from app.modules.signos_vitales.routes import router as signos_vitales_router
-# app.include_router(signos_vitales_router, prefix="/signos_vitales", tags=["signos_vitales"])
+from app.modules.signos_vitales.routes import router as signos_vitales_router
+app.include_router(signos_vitales_router, prefix="/signos_vitales", tags=["signos_vitales"])
 
 from app.modules.medicos.routes import router as medicos_router
 app.include_router(medicos_router, prefix="/medicos", tags=["medicos"])
---
+++ app/modules/signos_vitales/schema.py
@@ -0,0 +1,24 @@
+from pydantic import BaseModel
+from typing import Optional
+
+
+class SignosVitalesCreate(BaseModel):
+
+    peso: float
+    estatura: float
+    temperatura: float
+    presion_arterial: str
+    frecuencia_cardiaca: int
+    saturacion_oxigeno: int
+    id_historia_clinica_fk: int
+    id_consulta_fk: Optional[int] = None
+
+
+class SignosVitalesResponse(BaseModel):
+
+    peso: float
+    estatura: float
+    temperatura: float
+    presion_arterial: str
+    frecuencia_cardiaca: int
+    saturacion_oxigeno: int
