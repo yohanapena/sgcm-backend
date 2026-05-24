f32907fb7de33db1bf09c83f5c818e7f6f3055e8
Andrea Pena <yohanapena339@gmail.com>
2026-05-24 16:09:33 -0500

    fix: evitar duplicados en listar_citas agregando GROUP BY c.id_cita

--- app/modules/citas/repository.py
+++ app/modules/citas/repository.py
@@ -339,6 +339,9 @@ class CitaRepository:
         if condiciones:
             query += "\nWHERE " + " AND ".join(condiciones)
 
+        # Evitar duplicados cuando un médico tiene varias especialidades
+        query += "\nGROUP BY c.id_cita"
+
         query += "\nORDER BY c.fecha, c.hora"
 
         cursor.execute(query, tuple(valores))
